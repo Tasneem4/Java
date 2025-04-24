220402a2bec80492be7b28f0633da347e73f176c
tasneem bano <tasidtl432@gmail.com>
2025-04-24 08:06:33 +0530

    first commit

--- Java/src/basics/Methods.java
+++ Java/src/basics/Methods.java
@@ -4,7 +4,7 @@ public class Methods {
 
 	public static void main(String[] args) {
 		System.out.println("learning to use functions/methods");
-
+		System.out.println("exploring methods vs constructors");
 	}
 
 }
