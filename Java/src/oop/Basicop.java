05d99725b3b1ae0695819b9cf1b9b2dc629e4959
tasneem bano <tasidtl432@gmail.com>
2025-08-05 18:21:07 +0530

    fifth commit

--- Java/src/oop/Basicop.java
+++ Java/src/oop/Basicop.java
@@ -5,7 +5,7 @@ public class Basicop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		classBasicop s1= new classBasicop();
-		System.out.println(s1.yob);
+		
 		s1.yob=2002;
 		s1.name="Tasneem";
 		System.out.println(s1.yob+" "+s1.name);
